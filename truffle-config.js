e5820d7a7dccd8da7af8f1a9f8ca404c0b814a08
james <devoftree@gmail.com>
2021-12-15 08:01:30 -0300

    init

---
+++ truffle-config.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require('fs');
+const PRIVATE_KEY = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    dev: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    avax: {
+      provider: () => new HDWalletProvider(
+          {
+            privateKeys: [PRIVATE_KEY],
+            providerOrUrl: `wss://speedy-nodes-nyc.moralis.io/${process.env.MORALIS}/avalanche/mainnet/ws`,
+            addressIndex: 0
+          }),
+      network_id: 1,
+      networkCheckTimeout: 1000000,
+      confirmations: 3,
+      timeoutBlocks: 50000,
+      websocket: true,
+      skipDryRun: true
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  compilers: {
+    solc: {
+      version: "0.7.5",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    snowtrace: process.env.SNOWTRACE_API_KEY,
+  }
+
+};
